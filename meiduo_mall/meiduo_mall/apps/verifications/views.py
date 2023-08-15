20c1d14a7ba4b2e7d02ab9decae5df555ec9bb58
HEDSCD <2397029570@qq.com>
2023-08-15 22:45:42 +0800

    验证码之图形验证码-前端逻辑

--- meiduo_mall/meiduo_mall/apps/verifications/views.py
+++ meiduo_mall/meiduo_mall/apps/verifications/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import View
 from django_redis import get_redis_connection
 from django import http
+from . import constants
 
 from verifications.libs.captcha.captcha import captcha
 
@@ -25,7 +26,7 @@ class ImageCodeView(View):
         # 保存图形验证码
         redis_conn = get_redis_connection('verify_code')
         # redis_conn.setex('key', 'expires', 'value')
-        redis_conn.setex('img_%s' % uuid, 300, text)
+        redis_conn.setex('img_%s' % uuid, constants.IMAGE_CODE_REDIS_EXPIRES, text) # constants.IMAGE_CODE_REDIS_EXPIRES=300
 
         # 响应图形验证码
         return http.HttpResponse(image, content_type='image/jpg')
--- meiduo_mall/meiduo_mall/static/js/register.js
+++ meiduo_mall/meiduo_mall/static/js/register.js
@@ -13,6 +13,7 @@ var vm = new Vue({
         allow: '',
         image_code_url: '',
         uuid: '',
+        image_code: '',
 
         // v-show
         error_name: false,
@@ -20,10 +21,12 @@ var vm = new Vue({
         error_password2: false,
         error_mobile: false,
         error_allow: false,
+        error_image_code: false,
 
         // error_message
         error_name_message: '',
         error_mobile_message: '',
+        error_image_code_message: '',
     },
     mounted() { // 页面加载完会被调用的
         // 生成图形验证码
@@ -120,6 +123,15 @@ var vm = new Vue({
                     })
 
             }
+        },
+        // 校验图形验证码吗
+        check_image_code() {
+                if (this.image_code.length != 4) {
+                    this.error_image_code_message = '请填写图形验证码';
+                    this.error_image_code = true;
+                } else {
+                    this.error_image_code = false;
+                }
         },
         // 校验是否勾选协议
         check_allow() {
