a19b45dd2d53df33c9c0eabe05feedf62ea64154
HEDSCD <2397029570@qq.com>
2023-08-16 09:34:00 +0800

    验证码之图形验证码-前端逻辑

--- meiduo_mall/meiduo_mall/apps/users/__init__.py
+++ meiduo_mall/meiduo_mall/apps/users/__init__.py
@@ -1,3 +1,3 @@
 import pymysql
-pymysql.version_info = (3, 3, 13, "final", 0)
+# pymysql.version_info = (3, 3, 13, "final", 0)     # 根据mysqlclient模块的版本来的，如果版本是2.1.1就不用这行代码，如果是2.2.0就需要。
 pymysql.install_as_MySQLdb()
